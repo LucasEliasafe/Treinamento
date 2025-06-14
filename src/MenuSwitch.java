a57c82a18c7a536f341d8a8920b0b8f1fcbc9f92
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2025-06-14 18:52:27 -0300

    Feat: Criado o Switch

---
+++ src/MenuSwitch.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class MenuSwitch {
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("==Menu==");
+        System.out.println("1 - Dizer olá");
+        System.out.println("2 - Mostrar a data de hoje");
+        System.out.println("3 - Sair");
+
+        System.out.print(" Escolha uma opção (1,2 ou 3): ");
+        int opcao =scanner.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Seja bem-vindo");
+                break;
+
+            case 2:
+                System.out.println("Hoje é um ótimo dia para estudar java!");
+                break;
+
+            case 3:
+                System.out.println("Saindo do programa... até logo!");
+                break;
+            default:
+                System.out.println("Opção inválida. Tente 1, 2 ou 3.");
+                break;
+
+        }
+
+        scanner.close();
+
+
+    }
+
+}
